71903e5c0ba59c53ae23fb7becd8b88805194eb2
Alex Viktorov <viktorovaleksandr7@gmail.com>
2021-03-14 20:57:33 +0200

    debug js file

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,3 @@
 $(() => {
 	const controller = new TodosController();
 })
-
-
-// 1) в render попадает все todo под id 201 
-// 2) при обновлении todo новосозданіе падают вниз
-// 2) event.stopPropagation() не срабатывает.
