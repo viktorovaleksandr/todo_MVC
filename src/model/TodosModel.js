65a392604bc3903f40ceb711ef1387a741c97348
Alexander Viktorov <viktorovaleksandr7@gmail.com>
2021-03-16 17:12:33 +0200

    Update TodosModel.js

--- src/model/TodosModel.js
+++ src/model/TodosModel.js
@@ -5,17 +5,17 @@ class TodosModel {
 
 	async getTodos() {
      	return fetch('https://jsonplaceholder.typicode.com/todos')
-      .then((response) => response.json())
-      .then((todos) => this.todos = todos);
+      		.then((response) => response.json())
+      		.then((todos) => this.todos = todos);
    }
 
    async postNewTodo(newTodo) {
    	return fetch('https://jsonplaceholder.typicode.com/todos', {
-  			method: 'POST',
-  			body: JSON.stringify(newTodo),
-  			headers: {
+  		method: 'POST',
+  		body: JSON.stringify(newTodo),
+  		headers: {
     			'Content-type': 'application/json; charset=UTF-8',
-  			},
+  		},
 		})
   		.then((response) => response.json())
   		.then(newTodo => {
@@ -29,11 +29,11 @@ class TodosModel {
 		if (todo.id === id) todo.completed = !todo.completed;
 
    	return fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
-  			method: 'PUT',
-  			body: JSON.stringify(todo),
-  			headers: {
+  		method: 'PUT',
+  		body: JSON.stringify(todo),
+  		headers: {
     			'Content-type': 'application/json; charset=UTF-8',
-  			},
+  		},
 		})
   		.then((response) => response.json())
   		.then((toggleTodo) => {
@@ -48,11 +48,11 @@ class TodosModel {
 
    async deleteTodo(id,todoItem) {
    	return fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
-      method: 'DELETE',
+      	method: 'DELETE',
    	})
-		.then(() => {
-			todoItem.remove();
-			this.todos = this.todos.filter(todo => todo.id !== id);
-		});
+	.then(() => {
+		todoItem.remove();
+		this.todos = this.todos.filter(todo => todo.id !== id);
+	});
    }
 }
